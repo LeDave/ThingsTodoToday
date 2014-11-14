47d5a3bd800ce8c17db3cffb9ab1855e79b422a6
LeDave <alibaba7488@outlook.com>
2014-11-14 14:01:10 +0000

    Add Datepicker control

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,33 +1,39 @@
 'use strict';
 /**
- * @ngdoc function
+ * @ngdoc functiondiv>
  * @name workspaceApp.controller:MainCtrl
  * @description
  * # MainCtrl
  * Controller of the workspaceApp
  */
-angular.module('workspaceApp').controller('MainCtrl', function($scope, localStorageService) {
+angular.module('workspaceApp').controller('MainCtrl', function($scope, localStorageService,$filter) {
     var todosInStore = localStorageService.get('todos');
     $scope.todos = todosInStore || [];
     $scope.$watch('todos', function() {
         localStorageService.set('todos', $scope.todos);
     }, true);
-    $scope.addTodo = function() {
+    $scope.addTodo = function() { 
         if($scope.todos.indexOf($scope.todo) === -1 && !angular.isUndefined($scope.todo) && $scope.todo !== null && $scope.todo !== '') {
-            $scope.todos.push($scope.todo);
+            $scope.todos.push({date: $filter('date')($scope.t_date, 'yyyy.M.dd'), name: $scope.todo, done: 0});
         }
-        $scope.todo = '';
+        $scope.todo = ''; 
     };
     $scope.removeTodo = function(index) {
         $scope.todos.splice(index, 1);
     };
     
     $scope.today = function() {
-        $scope.dt = new Date();
+        $scope.t_date = new Date();
+        $scope.todaySelected = 1;
     };
     $scope.today();
-    
-    $scope.selectDate = function() {
-        alert($scope.dt);
+    $scope.maxDate = new Date();
+    $scope.matchDate = function(query) {
+      return function(todo) { return todo.date.match($filter('date')(query, 'yyyy.M.dd')); }
+    };
+    $scope.isToday = function() {
+        if($filter('date')($scope.t_date, 'yyyy.M.dd') == $filter('date')(new Date(), 'yyyy.M.dd'))
+            $scope.todaySelected = 1;
+        else $scope.todaySelected = 0;
     };
 });
